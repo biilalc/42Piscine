0f74e7498f19d622bfd8f5d8b0765fedb3490e16
Bilal Çayır <99775961+biilalc@users.noreply.github.com>
2023-02-25 22:15:02 +0300

    Update ft_point.h

--- c08/ex03/ft_point.h
+++ c08/ex03/ft_point.h
@@ -19,4 +19,18 @@ typedef struct s_point
 	int	y;
 }	t_point;
 
+/*
+	bu yapıyı bir veritabanı olarak düşünebilirsiniz
+	s_point adında bir struct oluşturduk ve buna iki adet int değer tanımladık
+
+	bunun bize ne yararı var : .c dosyasında kod yazarken her seferinde x ve y tanımlamaktan kurtulduk,
+	artık bu yapıyı çağırdığımızda hazırda iki adet int değerim gelecek
+	
+	örneğin .c dosyasında şu şekilde kullanılabilir
+	
+	t_point ornek;
+	ornek.x = 10;
+	ornek.y = 20;
+*/
+
 #endif
