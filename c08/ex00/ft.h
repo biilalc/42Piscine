97e140a297e6a71fd93a8d733deaa261c6bb4276
Bilal Çayır <99775961+biilalc@users.noreply.github.com>
2023-02-25 21:29:54 +0300

    Update ft.h

--- c08/ex00/ft.h
+++ c08/ex00/ft.h
@@ -10,13 +10,17 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_H
-# define FT_H
+#ifndef FT_H //FT_H tanımlanmamışsa,
+# define FT_H //FT_H'ı tanımla (tanımlandıysa bu satırı atlayacak)
 
 void	ft_putchar(char c);
-void	ft_swap(int *a, int *b);
-void	ft_putstr(char *str);
-int		ft_strlen(char *str);
+void	ft_swap(int *a, int *b);          //burada yapılan işlemlerin açıklaması : "ben bu fonksiyonları tekrar tekrar tekrar kullanmak istiyorum,
+void	ft_putstr(char *str);             //her seferinde fonksiyonları yazmak istemiyorum, prototiplerini buraya tanımladım,
+int		ft_strlen(char *str);             //çağırdığım zaman direkt gel beni yorma"
 int		ft_strcmp(char *s1, char *s2);
 
 #endif
+
+//kısaca .h dosyaları bizim işimizi kolaylaştıran bir fonksiyon kütüphanesidir.
+//.c dosyalarında kodların üstüne #include "ft.h" yazdığımızda .h dosyasında tanımlanan fonksiyonları kullanabiliriz
+//printf kullanmak istediğimiz zaman #include <stdio.h> yazmakla aynı şey
