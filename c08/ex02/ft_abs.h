1f2b085de1401f02978834afc279d012a8a59d2f
Bilal Çayır <99775961+biilalc@users.noreply.github.com>
2023-02-25 21:46:43 +0300

    Update ft_abs.h

--- c08/ex02/ft_abs.h
+++ c08/ex02/ft_abs.h
@@ -13,6 +13,19 @@
 #ifndef FT_ABS_H
 # define FT_ABS_H
 
-# define ABS(Value) (Value < 0 ? -Value : Value)
+# define ABS(Value) (Value < 0 ? -Value : Value) //Bunu if-else gibi düşünün. C dilinde buna üç operatör (ternary operator) denir. Aşağıda çalışma mantığı yazıyor
 
 #endif
+
+/*
+    Bu kod, bir sayının mutlak değerini hesaplamak için bir makro tanımlar. Makronun adı ABS'dir ve bir argüman olan Value parametresi alır.
+    
+    Makro tanımı, Value'nun 0'dan küçük olup olmadığını kontrol eder. Eğer Value 0'dan küçükse, o zaman (-Value) geri döndürülür. 
+    Aksi halde, yani Value 0 veya daha büyükse, Value değeri doğrudan geri döndürülür.
+
+    Bu makro, kullanıcıların kodlarında mutlak değer hesaplamak için bir işlev çağırmalarına gerek kalmadan,
+    doğrudan makro adını ve değeri kullanarak hesaplama yapmalarına olanak tanır. 
+    
+    Örneğin, ABS(-5) ifadesi -5'in mutlak değerini, yani 5'i hesaplayacaktır. 
+    Bu nedenle, bu makro kodun daha okunaklı ve daha kısa olmasını sağlar.
+*/
